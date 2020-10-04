df3206ad430d7bbbce5f50a4d3bae559c400abdf
89hikari <vladislav0151@bk.ru>
2020-10-04 21:42:12 +0300

    sync

--- concourse/atc/feature_flags.go
+++
@@ -1,9 +0,0 @@
-package atc
-
-var (
-	EnableGlobalResources                bool
-	EnableRedactSecrets                  bool
-	EnableBuildRerunWhenWorkerDisappears bool
-	EnableAcrossStep                     bool
-	EnablePipelineInstances              bool
-)
